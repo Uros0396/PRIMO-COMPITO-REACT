f2724341440c6dc0c27b42d77ad2cc0c39161808
uros milenkovic <168258764+Uros0396@users.noreply.github.com>
2024-09-12 05:06:34 +0200

    Add files via upload

---
+++ primo compito react/react.js
@@ -0,0 +1,65 @@
+/*1. Cos’è ReactJS?
+
+React e' una libreria, che serve alla creazione di interfacce utente, crea delle single page application. In react abbiamo dei componenti, che sono gli elementi fondamentali per costruire un' applicazione. In react avremo un altro tipo di sintassi che e' un mix tra JS e Html.
+
+Domanda aperta
+
+2. ReactJS è un framework, non una libreria.
+
+Vero / Falso   FALSO
+
+3. Il file package.json contiene…
+
+– Informazioni non essenziali, può essere tranquillamente eliminato
+
+– Solamente il nome e la versione dell’applicazione
+
+– Molte informazioni utili, come ad esempio l’elenco di tutte le dipendenze richieste dall’applicazione =>  CORRETTO
+
+– Il primo componente React caricato all’avvio
+
+4. create-react-app è l’unico modo possibile per creare un’applicazione React.
+
+Vero / Falso    FALSO
+
+5. Qual è il comando da lanciare nel terminale per creare una nuova create-react-app con nome “test”?
+
+– npx create-react-app test  CORRETTO
+
+– npm create-react-app test
+
+– npx create-react-app-test
+
+– npx create-new-react-app test
+
+6. Cos’è un componente React?
+
+– Una libreria che è necessario importare dentro l’applicazione
+
+– Una pagina ben specifica all’interno dell’applicazione
+
+– Un blocco di logica/contenuto riutilizzabile all’interno dell’applicazione => CORRETTO
+
+– Una landing page per l’applicazione
+
+7. Un componente React può venire creato in tre modi: come funzione, classe o interfaccia.
+
+Vero / Falso FALSO
+
+8. Le props sono frammenti di informazione assegnati all’invocazione di un componente React, utili al fine di rendere il componente dinamico e più riutilizzabile.
+
+Vero/Falso VERO
+
+9. Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario 
+
+Vero/Falso VERO
+
+10. Da dove possono venire recuperate le props all’interno di un componente React creato come classe?
+
+– Sono proprietà contenute in un oggetto accessibile attraverso i parametri della funzione
+
+– Possono essere recuperate all’interno dello oggetto ‘this’, dentro un sotto-oggetto chiamato ‘props’ => CORRETTO
+
+– Vengono applicate come proprietà dell’oggetto globale ‘window’
+
+– Vengono applicate come proprietà dell’oggetto ‘document’*/
